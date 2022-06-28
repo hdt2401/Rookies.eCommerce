18e8b2f296c99e155d27990acbd9b27fa4d24374
fastcoding_vn_bryan <bryan.fastcodingvn@gmail.com>
2022-06-29 00:44:28 +0700

    brand and category

---
+++ Rookies.eCommerce/Controllers/BrandController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rookies.eCommerce.Data;
+using Rookies.eCommerce.Domain;
+
+namespace Rookies.eCommerce.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: BrandController
+        public readonly EFContext _context;
+        public BrandController(EFContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("/brand")]
+        public IActionResult Index()
+        {
+            var brand = _context.Brands.ToList();
+            return View(brand);
+        }
+        [HttpGet("/brand/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost("/brand/create")]
+        public IActionResult Create(string name)
+        {
+            _context.Brands.Add(new Brand
+            {
+                Name = name
+            });
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Rookies.eCommerce/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rookies.eCommerce.Data;
+using Rookies.eCommerce.Domain;
+using System.Linq;
+
+namespace Rookies.eCommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly EFContext _context;
+        public CategoryController(EFContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("/category")]
+        public IActionResult Index()
+        {
+            var category = _context.Categories.ToList();
+            return View(category);
+        }
+        [HttpGet("/category/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost("/category/create")]
+        public IActionResult Create(string name)
+        {
+            _context.Categories.Add(new Category
+            {
+                Name = name,
+                ParentId = 1,
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now,
+            });
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //[HttpPost("/category/delete/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
