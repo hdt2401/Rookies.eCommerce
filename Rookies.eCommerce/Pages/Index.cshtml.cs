d43a36e815f4ace578c820bcea47e3d65e6bb1ad
fastcoding_vn_bryan <bryan.fastcodingvn@gmail.com>
2022-07-06 17:23:16 +0700

    show all product from api

--- Rookies.eCommerce/Pages/Index.cshtml.cs
+++ Rookies.eCommerce/Pages/Index.cshtml.cs
@@ -21,9 +21,12 @@ namespace Rookies.eCommerce.Client.Pages
             var _http = new HttpClient();
             _http.BaseAddress = new Uri("https://localhost:7276");
 
-            var res = await _http.GetAsync("api/Category");
+            var resCategory = await _http.GetAsync("api/Category");
+            var resultCategory = resCategory.Content.ReadAsStringAsync().Result;
+            DBCategory = JsonConvert.DeserializeObject<List<Rookies.eCommerce.Domain.Category>>(resultCategory);
+
+            var res = await _http.GetAsync("api/Product");
             var result = res.Content.ReadAsStringAsync().Result;
-            DBCategory = JsonConvert.DeserializeObject<List<Rookies.eCommerce.Domain.Category>>(result);
             DBProduct = JsonConvert.DeserializeObject<List<Rookies.eCommerce.Domain.Product>>(result);
 
             return Page();
