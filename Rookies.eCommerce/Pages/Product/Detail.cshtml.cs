2cb4095aede06444e28787d5ec173e31abeb5ff7
hdt2401 <thien12000@gmail.com>
2022-07-12 14:47:23 +0700

    Product Detail

--- Rookies.eCommerce/Pages/Product/Detail.cshtml.cs
+++ Rookies.eCommerce/Pages/Product/Detail.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Rookies.eCommerce.Pages.Product
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient _http;
+        private readonly ILogger<DetailModel> _logger;
+        public Rookies.eCommerce.Domain.Product product = new Rookies.eCommerce.Domain.Product();
+        public DetailModel(ILogger<DetailModel> logger)
         {
+            _logger = logger;
+        }
+        public async Task<ActionResult> OnGet(int id)
+        {
+            var _http = new HttpClient();
+            _http.BaseAddress = new Uri("https://localhost:7276");
+            var res = await _http.GetAsync($"api/Product/{id}");
+            var result = res.Content.ReadAsStringAsync().Result;
+            product = JsonConvert.DeserializeObject<Rookies.eCommerce.Domain.Product>(result);
+            return Page();
         }
     }
 }
--- Rookies.eCommerce/Pages/Product/Index.cshtml.cs
+++ Rookies.eCommerce/Pages/Product/Index.cshtml.cs
@@ -7,15 +7,15 @@ namespace Rookies.eCommerce.Pages.Product
     public class IndexModel : PageModel
     {
         private readonly HttpClient _http;
-        public List<Rookies.eCommerce.Domain.Product> DBCategory = new List<Rookies.eCommerce.Domain.Product>();
-        public async Task<IActionResult> OnGet()
+        public List<Rookies.eCommerce.Domain.Product> DBProduct = new List<Rookies.eCommerce.Domain.Product>();
+
+        public async Task<ActionResult> OnGetAsync()
         {
             var _http = new HttpClient();
             _http.BaseAddress = new Uri("https://localhost:7276");
-
             var res = await _http.GetAsync("api/Product");
             var result = res.Content.ReadAsStringAsync().Result;
-            DBCategory = JsonConvert.DeserializeObject<List<Rookies.eCommerce.Domain.Product>>(result);
+            DBProduct = JsonConvert.DeserializeObject<List<Rookies.eCommerce.Domain.Product>>(result);
 
             return Page();
         }
