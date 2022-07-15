aa4ab84fdda63c189062163bf018992aad6882ef
hdt2401 <thien12000@gmail.com>
2022-07-16 04:23:20 +0700

    delete redundant project from main solution

--- Rookies.eCommerce.Store/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Rookies.eCommerce.Store.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
